eabe859dd33b7058814d33d90e431c55293ce0c1
ygorp <ygor-nogueira@hotmail.com>
2023-03-11 09:19:44 -0300

    ...

--- cadastrousuario.php
+++ cadastrousuario.php
@@ -8,12 +8,12 @@
     <title>Cadastro - BMVIX</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/cadastro.css">
+    <link rel="stylesheet" href="css/cadastrousuario.css">
 </head>
 
 <body>
 
-    <section class="cadastro-usuario">
+    <section class="main">
         <form>
             <img src="img/logo.png" alt="">
             <h2>Cadastre-se aqui!</h2>
